202be42e097153a8f97b9b63e53658d9047d4885
Manjunath N P <mail.manjunathnp@gmail.com>
2018-07-03 10:02:58 +0530

    New codes added

--- src/com/javapractice/RemoveChar.java
+++ src/com/javapractice/RemoveChar.java
@@ -5,11 +5,16 @@ public class RemoveChar
 
 	public static void main(String[] args) 
 	{
-		String s1 = "!@#!@#! Hello @!#@!# World!";
+//		String s1 = "!@#!@#! Hello @!#@!# World!";
+//		
+//		//Regular Expression
+//		String s1Res = s1.replaceAll("[^a-zA-Z0-9' ']", "");
+//		System.out.println(s1Res);
 		
-		//Regular Expression
-		String s1Res = s1.replaceAll("[^a-zA-Z0-9' ']", "");
-		System.out.println(s1Res);
+		String str = "!@#!@#@Hello World!! !@@!#~!";
+		String newStr = str.replaceAll("[^a-zA-Z0-9' ' ]", "");
+		System.out.println(newStr);
+
 	}
 
 }
