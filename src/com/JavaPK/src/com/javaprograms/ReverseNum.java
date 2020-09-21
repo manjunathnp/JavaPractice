5d470b65771af6513c421d515b43da890fe9d0a4
Manjunath N P <31787102+manjunathnp@users.noreply.github.com>
2020-09-21 10:49:07 +0530

    Add files via upload

--- src/com/JavaPK/src/com/javaprograms/ReverseNum.java
+++ src/com/JavaPK/src/com/javaprograms/ReverseNum.java
@@ -10,14 +10,24 @@ public class ReverseNum {
 		System.out.println("Enter the number: ");
 		int num = sc.nextInt();
 		
-		int rev=0;
+		// Logic1 - Algorithm
+		/*int rev=0;
 		
 		while(num!=0)
 		{
 			rev = rev*10 + num%10;
 			num = num/10;
-		}
+		}*/
 		
+		//Logic2 - using StringBuffer Class
+		/*StringBuffer sb = new StringBuffer(String.valueOf(num));
+		StringBuffer rev = sb.reverse();*/
+		
+		
+		//Logic3 - using StringBuilder Class
+		StringBuilder sbl = new StringBuilder();
+		sbl.append(num);
+		StringBuilder rev = sbl.reverse();
 		System.out.println("Reverse Number is: "+rev);
 		
 		
